bf2026d570f65aadf377d7b64a95cea4a862b7ef
erika.ishida <erika.ishida@cleaveware.com>
2024-03-01 13:55:49 +0900

    snyk test

--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ setuptools.setup(
     name="dsv_data_science",
     version="0.1.0",
     author="Daikin U.S. Corporation",
-    author_email="josiah.johnston@dsv.daikin.com",
-    description="Assortment of python-based data science tools in a microservice.",
-    packages=setuptools.find_packages(include=[
-        "dsv_data_science", 
-        "dsv_data_science.*"
-    ]),
     python_requires='>=3.6',
     license='Unlicensed',
     install_requires=[
